989ae5d825a18076ab820fa0bca14cdb39aa188f
katarzynawilczak <kasia_9716@wp.pl>
2017-10-10 12:10:01 +0200

    lab1

--- lab1/macierz/Macierz.java
+++ lab1/macierz/Macierz.java
@@ -76,9 +76,9 @@ public class Macierz{
 		}
 		else{
 			int [][]result= new int[macierz.length][tab[0].length];
-			for(int i=0;i<macierz[0].length;i++){
-				for(int j=0;j<tab.length;j++){
-					for(int k=0; k<macierz.length;k++){
+			for(int i=0;i<macierz.length;i++){
+				for(int j=0;j<tab[0].length;j++){
+					for(int k=0; k<macierz[0].length;k++){
 						result[i][j]=result[i][j]+(macierz[i][k]*tab[k][j]);
 					}
 				}
